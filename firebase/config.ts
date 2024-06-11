8de4e8a173fe34fb17b72625bc3c98e5576f9ec3
Ekalabya Ghosh <154486133+not-ekalabya@users.noreply.github.com>
2024-06-11 16:51:22 +0530

    info privacy update

--- firebase/config.ts
+++ firebase/config.ts
@@ -4,13 +4,13 @@ import { GoogleAuthProvider } from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAYjUr1m79-YaYGga7or8ZpauT85Su86OM",
-  authDomain: "scalam-fire.firebaseapp.com",
-  projectId: "scalam-fire",
-  storageBucket: "scalam-fire.appspot.com",
-  messagingSenderId: "791843387602",
-  appId: "1:791843387602:web:aaf1e54ea9478b285b550b",
-  measurementId: "G-N6E2HXZKL8"
+  apiKey: "###",
+  authDomain: "###",
+  projectId: "###",
+  storageBucket: "###",
+  messagingSenderId: "###",
+  appId: "###",
+  measurementId: "###",
 };
 
 const app = initializeApp(firebaseConfig);
